0dcf2ce3a7111e93036b3e6c4e551f0bb81d20d3
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-06-28 15:35:24 +0530

    Time: 8 ms (65.05%), Space: 8.3 MB (41.47%) - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -10,49 +10,18 @@
  */
 class Solution {
 public:
-    /*method-2 function
-    void reverce(ListNode **head,ListNode *q,ListNode *p)
-    {
-        if(p)
-        {
-            reverce(head,p,p->next);
-            p->next=q;
-        }
-        else 
-        {
-            (*head)=q;
-        }
-    }*/
     ListNode* reverseList(ListNode* head) {
-        //method -2
-        //reverce(&head,NULL,head);
-        //return head;
-        
-        // method -1
-        /*
-        ListNode *p,*q,*r;
-        q=r=NULL;
+        ListNode*p,*q,*r;
         p=head;
+        q=NULL;
+        r=NULL;
         while(p)
         {
-        r=q;
-        q=p;
-        p=p->next;
-        q->next=r;
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
         }
         return q;
-        */
-        
-        //method -3
-        ListNode *new_node=NULL,*temp;
-        while(head)
-        {   
-            temp=head->next;
-            head->next=new_node;
-            new_node=head;
-            head=temp;
-        }
-        
-        return new_node;
-         }
+    }
 };
