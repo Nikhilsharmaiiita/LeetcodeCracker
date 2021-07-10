0a034ddc853ab65ff4c89c12214eeabfe8b37a17
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-10 23:47:02 +0530

    Time: 0 ms (100.00%), Space: 17.1 MB (36.39%) - LeetHub

---
+++ combination-sum/combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void fun(vector<int> &nums,int target,vector<vector<int>> &res,vector<int> &cont,int start,int sum)
+    {
+        //base case
+        if(target==sum)
+        {
+            res.push_back(cont);
+            return;
+        }
+        if(start==nums.size())
+            return;
+        //recusive step
+        //not include
+            fun(nums,target,res,cont,start+1,sum);
+        if(sum+nums[start]<=target)
+        {
+            cont.push_back(nums[start]);
+            fun(nums,target,res,cont,start,sum+nums[start]);
+            cont.pop_back();
+        }
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> cont;
+        fun(candidates,target,res,cont,0,0);
+        return res;
+    }
+};
