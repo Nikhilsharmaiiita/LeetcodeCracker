6dc289ba67e39cf81d589a34948261c353fc2d52
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-10 05:32:35 +0530

    Time: 40 ms (37.45%), Space: 17.8 MB (42.72%) - LeetHub

---
+++ combinations/combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void fun(int n,int k,vector<int> &cont,vector<vector<int>> &res,int start)
+    {
+        //base case
+        if(cont.size()==k)
+        {
+            res.push_back(cont);
+            return;
+        }
+        if(start==n+1)return;
+        //recursive solution 
+        cont.push_back(start);
+        fun(n,k,cont,res,start+1);
+        cont.pop_back();
+        fun(n,k,cont,res,start+1);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> cont;
+        fun(n,k,cont,res,1);
+        return res;
+    }
+};
