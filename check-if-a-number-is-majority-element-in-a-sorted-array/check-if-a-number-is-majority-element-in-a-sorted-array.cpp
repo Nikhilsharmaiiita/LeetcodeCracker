5833b12041de557af9dad34c866a9493a72c83ef
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-11 16:51:55 +0530

    Time: 8 ms (8.05%), Space: 7.5 MB (63.98%) - LeetHub

---
+++ check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+        return upper_bound(nums.begin(),nums.end(),target)-lower_bound(nums.begin(),nums.end(),target)>nums.size()/2;
+    }
+};
