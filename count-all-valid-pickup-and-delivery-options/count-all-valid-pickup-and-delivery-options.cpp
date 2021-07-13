153a7f47129ab6d7a01a2d3bd2487363e7266180
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-13 12:32:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.57%) - LeetHub

---
+++ count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        int res=1;
+        if(n==1)return res;
+        for(long int i=2;i<=n;i++)
+            res=(res*i*(2*i-1))%(1000000007);
+        
+        return res;
+    }
+};
