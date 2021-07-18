e603e49c7bfcd5eb2de58d2c01f6e84af692fb44
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-19 01:39:41 +0530

    Time: 244 ms (32.12%), Space: 146.7 MB (6.75%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -11,22 +11,21 @@
  */
 class Solution {
 public:
-    int minDepthHelper(TreeNode * root)
-    {
-        //base case
-        if(!root)return INT_MAX;
-         //for leaf node only
-        if(!root->left && !root->right)
-        {
-            return 1;
-        }
-        
-        //recursive step
-        return 1+min(minDepthHelper(root->left),minDepthHelper(root->right));
-    }
+    // int minDepthHelper(TreeNode * root)
+    // {
+    //     //base case
+    //     if(!root)return 0;
+    //     //recursive step
+    //     return 1+(min(minDepthHelper(root->left),minDepthHelper(root->right))?min(minDepthHelper(root->left),minDepthHelper(root->right)):max(minDepthHelper(root->left),minDepthHelper(root->right)));
+    // }
+    // int minDepth(TreeNode* root) {
+    //     //this for empty tree
+    //     if(!root)return 0;
+    //     return minDepthHelper(root);
+    // }
     int minDepth(TreeNode* root) {
-        //this for empty tree
-        if(!root)return 0;
-        return minDepthHelper(root);
-    }
+    if (!root) return 0;
+    int L = minDepth(root->left), R = minDepth(root->right);
+    return 1 + (min(L, R) ? min(L, R) : max(L, R));
+}
 };
