b3c8706d9cbcb879cc2c83663c1bdc6a6bab6422
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-20 00:31:30 +0530

    Time: 8 ms (53.84%), Space: 18.1 MB (21.53%) - LeetHub

--- sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,17 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void fun(TreeNode *root,vector<string> &res,string &s)
+    int ans;
+    int to_decimal(string s)
+    {
+            int path_sum=0;
+            int k=0;
+            for(int j=s.size()-1;j>=0;j--)
+            {
+                path_sum+=(pow(2,k)*(s[j]-'0'));
+                k++;
+            }
+        return path_sum;
+    }
+    
+    void fun(TreeNode *root,string &s)
     {
         //base case
         if(!root)
@@ -20,38 +22,24 @@ public:
         if(!root->left && !root->right)
         {
             s=s+to_string(root->val);
-            res.push_back(s);
+            ans+=to_decimal(s);
             s.pop_back();
             return;
         }
         //rec step
         s=s+to_string(root->val);
-        fun(root->left,res,s);
+        fun(root->left,s);
         s.pop_back();        
         s=s+to_string(root->val);
-        fun(root->right,res,s);
+        fun(root->right,s);
         s.pop_back();
     }
-        
-    
+           
     int sumRootToLeaf(TreeNode* root) {
-        vector<string> res;
         string s;
-        fun(root,res,s);
-        
-        //code for add them up all the element
-        int sum=0;
-        for(int i=0;i<res.size();i++)
-        {
-            int path_sum=0;
-            int k=0;
-            for(int j=res[i].size()-1;j>=0;j--)
-            {
-                path_sum+=(pow(2,k)*(res[i][j]-'0'));
-                k++;
-            }
-            sum+=path_sum;
-        }
-        return sum;
+        ans=0;
+        fun(root,s);
+
+        return ans;
     }
 };
