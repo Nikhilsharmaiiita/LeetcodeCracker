867180ba71e15f2e153fc328fd8a6c3b3803be98
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-24 01:59:43 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (28.34%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,23 +10,46 @@
  */
 class Solution {
 public:
+    //this is used for counting
+    int count(ListNode *head)
+    {
+        int sum=0;
+        while(head)
+        {
+            sum++;
+            head=head->next;
+        }
+        return sum;
+    }
+    
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* p1,*p2;
-        if(!head->next && n==1)return NULL;
-        p1=p2=head;
-        int x=n;
-        while(n>1)
-        {
-            p2=p2->next;
-            n--;
+        int cnt=count(head)-n;
+        if(cnt==0)return head->next;
+        ListNode *ans=head;
+        while(cnt>1)
+        {   cnt--;
+            head=head->next;
         }
-        while(p2->next&&p2->next->next)
-        {
-            p2=p2->next;
-            p1=p1->next;
-        }
-        if(!p2->next)head=head->next;
-        else p1->next=p1->next->next;
-        return head;
+        head->next=head->next?head->next->next:NULL;
+        return ans;
+        
+        
+        // ListNode* p1,*p2;
+        // if(!head->next && n==1)return NULL;
+        // p1=p2=head;
+        // int x=n;
+        // while(n>1)
+        // {
+        //     p2=p2->next;
+        //     n--;
+        // }
+        // while(p2->next&&p2->next->next)
+        // {
+        //     p2=p2->next;
+        //     p1=p1->next;
+        // }
+        // if(!p2->next)head=head->next;
+        // else p1->next=p1->next->next;
+        // return head;
     }
 };
