3c35d4de7bde799baceb73f004eb9a7cbdfcbe71
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-23 23:25:54 +0530

    Time: 8 ms (49.44%), Space: 13.7 MB (100.00%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        if(binary_search(nums.begin(),nums.end(),target))return true;
-        else false;
-        if(nums.size()==1)
+        int left = 0, right =  nums.size()-1, mid;
+        
+        while(left<=right)
         {
-            if(nums[0]==target)return true;else return false;
+            mid = (left + right) >> 1;
+            if(nums[mid] == target) return true;
+
+            // the only difference from the first one, trickly case, just updat left and right
+            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
+
+            else if(nums[left] <= nums[mid])
+            {
+                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
+                else left = mid + 1; 
+            }
+            else
+            {
+                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
+                else right = mid-1;
+            }
         }
-         int lo=0,hi=nums.size()-1,mid;
-        while(lo<hi)
-        {
-            mid=lo+(hi-lo)/2;
-                if(nums[0]>=nums[mid])hi=mid;
-            else lo=mid+1;
-        }
-        int drip_point=lo;
-        if(drip_point==0)drip_point++;
-        cout<<drip_point;
-        lo=0,hi=drip_point-1;
-        while(lo<hi)
-        {
-            mid=lo+(hi-lo)/2;
-                if(nums[mid]>=target)
-                    hi=mid;
-            else 
-                lo=mid+1;
-        }
-        if(nums[lo]==target)return true;
-        lo=drip_point;
-        hi=nums.size()-1;
-        while(lo<hi)
-        {
-            mid=lo+(hi-lo)/2;
-                if(nums[mid]>=target)
-                    hi=mid;
-            else 
-                lo=mid+1;
-        }
-        if(nums[lo]==target)return true;
         return false;
     }
 };
