ef29d2aad3dfc7b9b0f9461b77fd7d1caa76c52d
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-23 22:42:29 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (95.02%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -1,25 +1,15 @@
 
 class Solution {
 public:
-    int count(ListNode* head)
-    {
-        int sum=0;
-        while(head)
-        {
-            sum++;
-            head=head->next;
-        }
-        return sum;
-    }
-    
     ListNode* middleNode(ListNode* head) {
-        //cnt is number of element in linked list
-        int cnt=count(head);
-        //1 2 3 4 5 
-        //1 2 3 4 5 6 
-        int x=cnt/2;
-        while(x--)
-            head=head->next;
-        return head;
+        ListNode*slow,*fast;
+        slow=fast=head;
+        while(fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next;
+            fast=fast->next;
+        }
+        return slow;
     }
 };
