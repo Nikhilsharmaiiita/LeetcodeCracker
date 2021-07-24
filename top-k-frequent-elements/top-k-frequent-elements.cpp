89f53501afe7fca2c7f5ab18279f47e412928ff8
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-24 23:57:44 +0530

    Time: 12 ms (92.76%), Space: 14.1 MB (10.99%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> m;
+        
         for(int i=0;i<nums.size();i++)
         {
             m[nums[i]]++;
         }
-        //heap type ? Max Heap
-        //mean heap 1 2 3 4 5
-        //1 2 3 4 5
-        //we want 5 4 3 2 1
+        
+        // 1 2 3 4 5 --> -1 -2 -3 -4 -5 --> -5 -4 -3 -2 -1
         multiset<pair<int,int>> pq;
         for(auto it:m)
         {
-            pq.insert({it.second,it.first});
+            pq.insert({-it.second,it.first});
         }
+        
         vector<int> v;
         for(int i=0;i<k;i++)
-        {
-            pair<int,int> x=*(--pq.end());
-            v.push_back(x.second);
-            pq.erase(--pq.end());
+        { 
+            v.push_back((*pq.begin()).second);
+            pq.erase(pq.begin());
         }
+        
         return v;
     }
 };
