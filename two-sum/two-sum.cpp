3d8deda5709f124a24c680d31e00aa6ea10d7187
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-30 19:47:40 +0530

    Time: 8 ms (95.69%), Space: 10.9 MB (37.10%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //O(n) solution with map space 
-        
-//         vector<int> v;
-//         // unordered_map<int,int> m;
-//         int n=nums.size();
-        
-//         for(int i=0;i<n;i++)
-//         {
-//             if(m.find(target-nums[i])!=m.end())
-//             {
-//                 v.push_back(m[target-nums[i]]);
-//                 v.push_back(i);
-//                 return v;
-//             }
-//             m[nums[i]]=i;
-//         }
-//         return v;
-        
-        //nlogn solution with O(n) space.
-        vector<vector<int>> v;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-            v.push_back({nums[i],i});
-        
-        sort(v.begin(),v.end());
-        
-        int i=0,j=n-1;
-        while(i<j)
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
         {
-            if(v[i][0]+v[j][0]==target)
-                return {v[i][1],v[j][1]};
-            else if(v[i][0]+v[j][0]>target)
-                j--;
-            else
-                i++;
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                return {i,mp[target-nums[i]]};
+            }
+            mp[nums[i]]=i;
         }
-        return {-1,-1};
+        return {};
     }
 };
