3c9a6de6fb43a0cc7945e753407b738b0eefccec
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-07-31 22:28:19 +0530

    Time: 7 ms (85.68%), Space: 8.3 MB (56.01%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,11 +6,9 @@ public:
         vector<int> freq(256,0);
         int max_win=INT_MIN;
         int i=0;
-        int flag=0;
         while(i<n)
         {
             freq[s[i]]++;
-
             if(freq[s[i]]>1)
             {
                 max_win=max(max_win,i-j);
@@ -19,8 +17,8 @@ public:
                     freq[s[j]]--;
                     j++;
                 }
-                flag=1;
             }
+            else
             max_win=max(max_win,i-j+1);
             i++;
         }
