deef511590a5e309eecc14bb033e1fdb2eb672af
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-11 18:54:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (65.04%) - LeetHub

--- reorganize-string/reorganize-string.cpp
+++ reorganize-string/reorganize-string.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        priority_queue<pair<int,char>> pq;
+        string ans;
         unordered_map<char,int> m;
-        for(int i=0;i<s.size();i++)
-            m[s[i]]++;
-        
-        //fill the priority queue
+        for(auto it:s)
+        m[it]++;
+        priority_queue<pair<int,char>> pq;
         for(auto it:m)
-            pq.push({it.second,it.first});
-        
-        string ans="";
-        while(!pq.empty())
         {
-            char x1=pq.top().second;
-            ans+=pq.top().second;
-            int x2=pq.top().first;
+            pq.push({it.second,it.first});
+        }
+        while(pq.size())
+        {
+            int x1=pq.top().first;
+            int x2=pq.top().second;
+            ans+=x2;
+            x1--;
             pq.pop();
-            x2--;
-            if(!pq.empty())
+            if(pq.size())
             {
-                char y1=pq.top().second;
-                ans+=pq.top().second;
-                int y2=pq.top().first;
+                int y1=pq.top().first;
+                int y2=pq.top().second;
+                ans+=y2;
+                y1--;
                 pq.pop();
-                y2--;
-                if(y2)
+                if(y1)
                 {
-                    pq.push({y2,y1});
+                    pq.push({y1,y2});
                 }
-            }else if(x2)return "";
-            if(x2)
-             pq.push({x2,x1}); 
+            }else if(x1)return "";
+            if(x1)pq.push({x1,x2});
         }
         return ans;
     }
