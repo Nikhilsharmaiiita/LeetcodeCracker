98de291e140f72ae1ddc5ce5f745f94df7604e0a
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-11 16:48:32 +0530

    Time: 164 ms (5.08%), Space: 7.9 MB (96.84%) - LeetHub

--- sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,24 +1,13 @@
-
 class Solution {
 public:
-    string frequencySort(string s) {   
-        unordered_map<char,int> freq;
-        for(int i=0;i<s.size();i++)
-           freq[s[i]]++;
-        multiset<pair<int,char>> st;
-        for(auto it:freq)
-        {
-            st.insert({-it.second,it.first});
-        }
-        int i=0;
-        for(auto it : st)
-        {
-            int x=abs(it.first);
-            while(x--)
-            {
-                s[i++]=it.second;
-            }
-        }
+    string frequencySort(string s) {
+        unordered_map<char,int> m;
+        for(auto it:s)m[it]++;
+        sort(s.begin(),s.end(),[&m](char a,char b){
+            if(m[a]==m[b])
+             return a>b;
+            return m[a]>m[b];
+        });
         return s;
     }
 };
