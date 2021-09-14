75e6ff00e7d91c56d2a469005120fa7fdcb74c4d
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-14 13:43:45 +0530

    Time: 4 ms (57.35%), Space: 11 MB (21.13%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-void fun(vector<int> &nums,int start,vector<vector<int>> &res,vector<int> &cont)
-{
-    //base case
-    if(start==nums.size())
+    void fun(vector<int> &nums,int start,vector<vector<int>> &res,vector<int>& cont)
     {
-        res.push_back(cont);
-        cout<<cont.size()<<" ";
-        return;
+        //base case
+        if(start==nums.size())
+        {
+            res.push_back(cont);
+            return;
+        }
+        
+        //rec step
+        fun(nums,start+1,res,cont);
+        cont.push_back(nums[start]);
+        fun(nums,start+1,res,cont);
+        cont.pop_back();
     }
-    //rec step
-    fun(nums,start+1,res,cont);
-    cont.push_back(nums[start]);
-    fun(nums,start+1,res,cont);
-    cont.pop_back();
-}
-vector<vector<int>> subsets(vector<int> &A) {
-    vector<vector<int>> res;
-    vector<int> cont;
-    fun(A,0,res,cont);
-    return res;
-}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> cont;
+        fun(nums,0,res,cont);
+        return res;
+    }
 };
