ffa9a42132a6ea7ab59aecb710505af6a917fed2
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-14 18:46:37 +0530

    Time: 3 ms (94.41%), Space: 8.6 MB (81.21%) - LeetHub

--- permutations-ii/permutations-ii.cpp
+++ permutations-ii/permutations-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-  void f(vector<int> &nums,int j,vector<bool> visited,vector<int> &cont,vector<vector<int>> &res)
+    void fun(vector<vector<int>> &res, vector<int> &cont,vector<int> & nums,vector<int> &vis)
     {
         //base case
         if(cont.size()==nums.size())
@@ -8,27 +8,28 @@ public:
             res.push_back(cont);
             return;
         }
-      int prev=-11;
+        //rec step
+        int prev=INT_MIN;
         for(int i=0;i<nums.size();i++)
         {
             if(prev==nums[i])continue;
-            if(!visited[i]){
-            visited[i]=true;
-            cont.push_back(nums[i]);
-            prev=nums[i];
-            f(nums,j+1,visited,cont,res);
-            cont.pop_back();
-            visited[i]=false;
-            }          
+            if(vis[i]==0)
+            {
+                cont.push_back(nums[i]);
+                vis[i]=1;
+                prev=nums[i];
+                fun(res,cont,nums,vis);
+                cont.pop_back();
+                vis[i]=0;
+            }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<int> cont;
         vector<vector<int>> res;
+        vector<int> cont;
+        vector<int> vis(nums.size(),0);
         sort(nums.begin(),nums.end());
-        vector<bool> visited(nums.size(),false);
-        f(nums,0,visited,cont,res);
-       
+        fun(res,cont,nums,vis);
         return res;
     }
 };
