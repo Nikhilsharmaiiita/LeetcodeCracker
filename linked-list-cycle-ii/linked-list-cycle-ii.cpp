aad4449a95bd183535c9e82d3e88c789582a2225
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-16 16:40:39 +0530

    Time: 11 ms (30.34%), Space: 7.5 MB (73.68%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,35 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-//this is using copying the address of the element 
+//     ListNode *detectCycle(ListNode *head) {
+//         ListNode *slow=head,*fast=head;
+//         if(!head||!head->next)return NULL;
+//         do
+//         {
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }while(fast&&fast->next&&slow!=fast);
+     
+//         //now testing there is a cycle or not in a link list 
+//         if(!fast || !fast->next)
+//             return NULL;
 
-//2nd method:- using slow fast pointer
-
-
- ListNode *detectCycle(ListNode *head) {
-     set<ListNode*> s;
-     while(head)
-     {
-         if(s.find(head)!=s.end())
-         return head;
-         s.insert(head);
-         head=head->next;
-     }
-     return NULL;
-        // ListNode *slow=head,*fast=head;
-        // if(!head||!head->next)return NULL;
-        // do
-        // {
-        //     slow=slow->next;
-        //     fast=fast->next->next;
-        // }while(fast&&fast->next&&slow!=fast);
-        // //now testing there is a cycle or not in a link list 
-        // if(!fast || !fast->next)
-        //     return NULL;
-        // fast=head;
-        // while(fast!=slow)
-        // {
-        //     slow=slow->next;
-        //     fast=fast->next;
-        // }
-        // return fast;
- }};
+//         fast=head;
+//         while(fast!=slow)
+//         {
+//             slow=slow->next;
+//             fast=fast->next;
+//         }
+//         return fast;
+//  }
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head->next)return NULL;
+        ListNode*slow,*fast;
+        slow=fast=head;
+        do
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        while(fast&&fast->next&&slow!=fast);
+        
+        if(!fast || !fast->next)
+            return NULL;
+            fast=head;
+        while(fast!=slow)
+            {slow=slow->next;
+            fast=fast->next;}
+        return fast;
+    }
+};
