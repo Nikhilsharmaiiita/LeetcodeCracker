221f210fc5669a157c90cc1fc039c696f521a0e4
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-16 20:21:03 +0530

    Time: 68 ms (7.30%), Space: 70.6 MB (97.20%) - LeetHub

--- add-two-numbers-ii/add-two-numbers-ii.cpp
+++ add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -1,16 +1,6 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* rev(ListNode* root)
+       ListNode* rev(ListNode* root)
     {
         ListNode *p,*q,*r;
         q=r=NULL;
