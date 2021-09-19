ab4b2f76b7079f64568a7726b1fbb2ac3c9464e3
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-19 14:59:24 +0530

    Time: 248 ms (30.86%), Space: 11.8 MB (55.24%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    bool SubSetSum(vector<int> &nums ,int sum)
-    {
-        vector<vector<int>> dp(nums.size()+1,vector<int> (sum+1,false));
-        int n=nums.size()+1;
-        for(int i=0;i<n;i++)
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(auto it:nums)sum+=it;
+        if(sum%2)return false;
+        sum=sum/2;
+        int n=nums.size();
+        vector<vector<bool>> dp(n+1,vector<bool> (sum+1,false));
+        for(int i=0;i<=n;i++)dp[i][0]=true;
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<sum+1;j++)
+            for(int j=1;j<=sum;j++)
             {
-                if(i==0 && j>0)dp[i][j]=false;
-                else if(j==0)dp[i][j]=true;
-                else if(nums[i-1]<=j)
-                {
+                if(nums[i-1]<=j)
                     dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
-                }
                 else dp[i][j]=dp[i-1][j];
             }
         }
-        return dp[n-1][sum];
+        return dp[n][sum];
     }
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];
-        if(sum%2==1)return false;
-        else 
-        {
-            return SubSetSum(nums,sum/2);
-        }
-        }
 };
