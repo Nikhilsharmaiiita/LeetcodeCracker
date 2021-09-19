24bcdc91245579b875ad0b045675ef4b5ef5f099
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-19 18:27:18 +0530

    Time: 20 ms (73.62%), Space: 13.4 MB (56.34%) - LeetHub

--- repeated-substring-pattern/repeated-substring-pattern.cpp
+++ repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-          string text=s+s;
-          string pattern=text.substr(1,text.size()-2);
-          int x=pattern.find(s);
+        string text1=s+s;
+        string text2=text1.substr(1,text1.size()-2);
+        int x=text2.find(s);
         if(x==string::npos)return false;
         return true;
     }
