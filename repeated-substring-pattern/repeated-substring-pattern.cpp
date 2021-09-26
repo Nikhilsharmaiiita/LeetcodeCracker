c52460c35fae64f797c9d376f7dc5e0bc83cfbcd
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-09-26 23:52:29 +0530

    Time: 24 ms (53.62%), Space: 16.7 MB (31.40%) - LeetHub

--- repeated-substring-pattern/repeated-substring-pattern.cpp
+++ repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -3,8 +3,29 @@ public:
     bool repeatedSubstringPattern(string s) {
         string text1=s+s;
         string text2=text1.substr(1,text1.size()-2);
-        int x=text2.find(s);
-        if(x==string::npos)return false;
-        return true;
+        int x=s.size();
+        vector<int> LPS(x,0);
+        int j=0;
+        
+        //LPS Array 
+        for(int i=1;i<x;i++)
+        {
+            j=LPS[i-1];
+            while(j>0 && s[i]!=s[j])
+            j=LPS[j-1];
+            if(s[i]==s[j])LPS[i]=j+1;
+        }
+        //KMP
+        int i=0;
+        j=0;
+        int m=text2.size();
+        while(i<m)
+        {
+            while(i<m && j<x && text2[i]==s[j]){i++;j++;}
+            if(j==x)return true;
+            if(j==0)i++;
+            else j=LPS[j-1];
+        }
+        return false;
     }
 };
