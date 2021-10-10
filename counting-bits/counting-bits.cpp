b3166ea9164a92ad4579ac6b5a8ac902dd8cc3bd
nikhil sharma <74041554+Nikhilsharmaiiita@users.noreply.github.com>
2021-10-11 02:23:39 +0530

    Time: 4 ms (88.01%), Space: 7.9 MB (71.43%) - LeetHub

---
+++ counting-bits/counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1,0);
+        if(n==0)return {0};
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        dp[i]=i%2==0?dp[i/2]:dp[i/2]+1;
+        return dp;
+    }
+};
